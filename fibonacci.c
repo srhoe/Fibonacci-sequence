6a29d01825cb11dc2eb6af7e1ae03b9ea81abb8b
Kevin Roberts <pirooted@icloud.com>
2024-05-05 09:34:29 -0400

    fibonacci sequence in C

---
+++ fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main(){
+    int n1 = 0, n2 = 1, n3, count, num;
+
+    printf("Enter the number of terms to be printed\n");
+    scanf("%d", &num);
+
+    printf("\n%d\n%d\n",n1, n2);
+
+    for(count = 3; count <= num; count++){
+        n3 = n1 + n2;
+        printf("%d\n", n3);
+        n1 = n2;
+        n2 = n3;
+    }
+
+    return 0;
+
+}

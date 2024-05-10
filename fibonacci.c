2849d2ffe1e7efe8124a4cac7246f4594ed79bd7
Kevin Roberts <pirooted@icloud.com>
2024-05-10 17:25:22 -0400

    Update fibonacci.c
    Removed n3, and replaced the count into the for loop, then added n1 + n2 in the print statement on line 9, and update n1 by adding n2, update n2 by subtracting the previous value of n2.

--- fibonacci.c
+++ fibonacci.c
@@ -1,20 +1,13 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main(){
-    int n1 = 0, n2 = 1, n3, count, num;
+    int n1 = 1, n2 = 0;
 
-    printf("Enter the number of terms to be printed\n");
-    scanf("%d", &num);
+    printf("%d\n%d\n",n1, n2);
 
-    printf("\n%d\n%d\n",n1, n2);
-
-    for(count = 3; count <= num; count++){
-        n3 = n1 + n2;
-        printf("%d\n", n3);
-        n1 = n2;
-        n2 = n3;
+    for(int count = 3; count <= 10; count++){
+        printf("%d\n",n1 + n2);
+        n2 = (n1 += n2) - n2;
     }
-
     return 0;
-
 }
